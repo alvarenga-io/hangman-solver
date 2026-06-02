34e7d7262e7807407217618c35884b938b3b44b7
alvarenga-io <alvxrenga.isa@gmail.com>
2026-06-02 10:46:18 -0300

    Alterei o nome do modulo para combinar com o nome do arquivo

--- prolog/estado.pl
+++ prolog/estado.pl
@@ -1,4 +1,4 @@
-:- module(state, [iniciar_partida/2, categoria/1, tamanho/1, candidato/1, tentadas/1, padrao/1, vidas/1]); %cria o modulo e exporta os predicados para que outro arquivos possam usá-los
+:- module(estado, [iniciar_partida/2, categoria/1, tamanho/1, candidato/1, tentadas/1, padrao/1, vidas/1]); %cria o modulo e exporta os predicados para que outro arquivos possam usá-los
 
 % predicados dinâmicos que registram o estado atual do jogo, são modificados durante a execução (assert/retract)
 :- dynamic categoria/1.
@@ -21,6 +21,6 @@ iniciar_partida(Categoria, Tamanho) :-
     assert(tentadas([])),
     assert(vidas(6)),
     length(Padrao, Tamanho),maplist (=(_),), %criar uma lista chamada Padrao com n de elementos igual à "Tamanho" . O maplist força todos os elementos na linha a serem _ 
-    assert(padrao(Padrao)),
+    assert(padrao(Padrao)).
 
     
